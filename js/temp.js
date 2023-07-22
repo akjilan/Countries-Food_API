47b0a3ccf3402d22898dc9b227e40dd75a399325
akjilan <jilann.aj@gmail.com>
2023-07-22 14:13:40 +0600

    updated

--- js/temp.js
+++ js/temp.js
@@ -1,15 +1,4 @@
 //my key : 35fddb0bee1f7d03670c583790324ad6
-//display with mouse enter:
-document
-.getElementById("inputWeatherID")
-.addEventListener("keypress", function (event) {
-if (event.key === "Enter") {
-  event.preventDefault();
-  displayTempAndCity();
-}
-});
-
-
 
 const loadTemperature = (myCity) => {
   const api_key = `4444c4aba5ed98c5a234f4dcd2400454`;
@@ -21,46 +10,37 @@ const loadTemperature = (myCity) => {
 
 const displayTemp = (data) => {
   console.log(data);
+  const cityName = document.getElementById("cityName");
   const displayTempId = document.getElementById("displayTempId");
+  const weatherDisplay = document.getElementById("weatherDisplay");
+  cityName.innerText = data.name;
   displayTempId.innerText = data.main.temp;
+  weatherDisplay.innerText = data.weather[0].main;
 };
 
 const displayTempAndCity = () => {
   const inputField = document.getElementById("inputWeatherID");
   const inputFieldText = inputField.value;
-  const cityName = document.getElementById("cityName");
   if (inputFieldText === "") {
     alert("give the name of the city");
   } else {
-    cityName.innerText = inputFieldText;
     loadTemperature(inputFieldText);
-    
   }
 };
+//display with mouse enter:
+document
+  .getElementById("inputWeatherID")
+  .addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      displayTempAndCity();
+    }
+  });
+
+//display with button:
 
 document
   .getElementById("weatherSearchButton")
   .addEventListener("click", function () {
     displayTempAndCity();
   });
-
-
-
-
-
-
-
-
-/////////////
-// const inputElement = document.getElementById("myInput");
-
-// inputElement.addEventListener("keyup", function() {
-//   const inputValue = inputElement.value.trim();
-
-//   if (inputValue === "") {
-//     alert("Please enter a value in the input field.");
-//   } else {
-//     // Perform further actions when input is not empty
-//     console.log("Input value:", inputValue);
-//   }
-// });
